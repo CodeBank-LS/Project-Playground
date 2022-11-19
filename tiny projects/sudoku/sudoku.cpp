27824fb459647c5a9962dd00f6c5cc1895e2ae76
CodeX-LS <80232744+CodeX-LS@users.noreply.github.com>
2022-11-20 01:46:30 +1030

    dfas

--- tiny projects/sudoku/sudoku.cpp
+++ tiny projects/sudoku/sudoku.cpp
@@ -1,12 +1,22 @@
 //A sudoku game that take input to generate board
 //check the value fill in grid
 //and solve the game by itself
+//test case
+//530070000
+//600195000
+//098000060
+//800060003
+//400803001
+//700020006
+//060000280
+//000419005
+//000080079
+
 #include <iostream>
-#include<stdio.h>
 using namespace std;
 int P[9][9];
 bool flag = false;
-// 处理输入 
+// Input handling
 void input(){
     char t;
     cout << "enter board，use 0 as empty\n";
@@ -34,27 +44,27 @@ void output(){
 }
 
 //fill in value in row n and column m and check the grid
-bool checkGrid(int m, int n, int val){
+bool checkGrid(int row, int column, int number){
     //row check
     for(int i = 0; i < 9; i ++){
-        if(P[n/9][i] == val){
+        if(P[row/9][i] == number){
             cout<<"same value in the row"<<endl;
             return false;
         }
     }
     //column check
     for(int i = 0; i < 9; i ++){
-        if(P[n/9][i] == val){
-            cout<<"same value in the row"<<endl;
+        if(P[column/9][i] == number){
+            cout<<"same value in the column"<<endl;
             return false;
         }
     }
     //grid check
-    int x = n / 9 / 3 * 3;
-    int y = n % 9 / 3 * 3;
+    int x = row / 9 / 3 * 3;
+    int y = column % 9 / 3 * 3;
     for(int i = 0; i < 3; i ++){
         for(int j = 0; j < 3; j ++){
-            if(P[x+i][y+j] == val){
+            if(P[x+i][y+j] == number){
                 cout<<"same value in the square"<<endl;
                 return false;
             }
@@ -125,18 +135,38 @@ int main(){
     cout << endl << endl;
     output();
     cout<<endl;
-    int n, m, val;
-    cout<<"enter row number, column number and value, seperated with space (e.g, 2 3 5 means fill 5 in row 2 and column 3:"<<endl;
-    cin>>n>>m>>val;
-    if((n>=0&&n<=9)&&(m>=0&&m<=9)&&(val>=1&&val<=9)){ 
-
-
+    cout<<"You want to solve the game by yourself? (y/n)"<<endl;
+    char t;
+    cin >> t;
+    if(t == 'y'||t == 'Y'){
+        cout<<"enter the number you want to fill in"<<endl;
+        int row,col,num;
+        cin >> row>>col>>num;
+        while(checkGrid(row,col,num)!=true){
+            P[row][col] = num;
+            output();
+            cout<<"enter the number you want to fill in"<<endl;
+            cin >> row>>col>>num;
         }
+
+
+
+    }
+    if(t == 'n'||t == 'N'){
+        cout<<"Do you want system solve it for you? (y/n)"<<endl;
+        char z;
+        cin >> z;
+        if(z == 'y'||z == 'Y'){
+            dfs(0);
+            output();
+        }else{
+            cout<<"Bye"<<endl;
+        }
+    }
+    return 0;
     }
 
 
 
-    //dfs(0);
-    output();
-    return 0;
-} 
+
+
