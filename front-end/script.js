9df2b68ea93b5f726ce69e1b5dda31d9babf8aac
CodeX-LS <80232744+CodeX-LS@users.noreply.github.com>
2023-01-02 23:32:50 +1030

    op

---
+++ front-end/script.js
@@ -0,0 +1,34 @@
+// var teachName="LS";
+
+
+// if(teachName=="LS"){
+//     let apples=1;
+//     const height="160cm";
+//     console.log("I have", apples, "apples");
+
+// }
+
+// const name="LS";
+// let profession="programmer";
+// var total=14;
+
+// const list=["item 1", 2, "item  2", 3.14];
+
+// const thing=null;//memory with nothing in it
+
+// const notDefined=undefined;
+
+// const getAge=function(){
+//     return 32;
+// };
+
+// const obj={
+//     "name":"Lin Su",
+//     "profession":"student"
+// }
+function addnumbers(pra1,pra2){
+    const total=Number(pra1)+Number(pra2);
+    return total;
+}
+
+const result=addnumbers("1","9");
