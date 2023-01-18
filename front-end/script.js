c4b06147c2aee03dc285c8a68488c08c0afbb637
CodeX-LS <80232744+CodeX-LS@users.noreply.github.com>
2023-01-18 13:58:52 +1030

    Api fetch()

--- front-end/script.js
+++ front-end/script.js
@@ -1,6 +1,8 @@
 // var teachName="LS";
 
 
+
+
 // if(teachName=="LS"){
 //     let apples=1;
 //     const height="160cm";
@@ -25,10 +27,69 @@
 // const obj={
 //     "name":"Lin Su",
 //     "profession":"student"
+// // }
+// function hello(){
+//     return "hello linsu"
 // }
-const num=1234;
-if(num===1234){
-    const greeting ="hello 1234";
-    console.log(greeting)
-}
+// const greet=hello()
+// console.log(greet)
 
+// const hlo=()=>{
+//     console.log("hello ls")
+// }
+// hlo()
+
+// const hllo=()=>"hello from one line function"
+// hllo()
+
+// const newgreet=(name)=>{
+//     console.log(`welcome ${name}`)
+// }
+// newgreet("sudalin")
+// class Person{
+//     constructor(name){
+//         this.name=name 
+//     }
+//     greeting(){
+//         console.log(`Hello from ${this.name}`)
+//     }
+//     setName(name){
+//         this.name=name
+//     }
+// }
+
+// const LS=new Person("lin su")
+
+// LS.greeting()
+// LS.setName("Da Lin")
+// LS.greeting()
+
+// fetch('https://swapi.dev/api/people/1/')
+//     .then(response=>response.json())
+//     .then(data=>{
+//         console.log(data)
+//     })
+const name=document.getElementById("name");
+const height=document.getElementById("height");
+const eyecolor=document.getElementById("eyecolor");
+const birthyear=document.getElementById("birthyear");
+
+const bttn=document.querySelector(".getRandomCharacter");
+    bttn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    name.innerHTML='<em>Loading...</e>';
+    height.innerHTML='<em>Loading...</e>';
+    eyecolor.innerHTML='<em>Loading...</e>';
+    birthyear.innerHTML='<em>Loading...</e>';
+
+const randNum=Math.ceil(Math.random()*83);
+fetch(`http://swapi.dev/api/people/${randNum}`)
+    .then(response=>response.json())
+    .then(charactor=>{
+        console.log(charactor)
+        name.innerHTML=charactor['name'];
+        height.innerHTML=charactor['height'];
+        eyecolor.innerHTML=charactor['eye_color'];
+        birthyear.innerHTML=charactor['birth_year']
+    })
+})
