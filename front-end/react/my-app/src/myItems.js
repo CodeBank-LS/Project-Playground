45d9641323bb125e26e13582ae139258d581a073
CodeX-LS <80232744+CodeX-LS@users.noreply.github.com>
2023-02-01 12:59:23 +1030

    热啊

---
+++ front-end/react/my-app/src/myItems.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class Item extends React.Component{
+    clickMe(){
+        console.log(" I am clicked",this.props.name)
+    }
+    render(){
+      return(
+        <h1 onClick={()=>this.clickMe()}>Hello from {this.props.name}</h1>
+      )
+    }
+  }
+
+  export default Item;
