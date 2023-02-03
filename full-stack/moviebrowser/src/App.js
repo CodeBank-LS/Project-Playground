092fe59f1572aec24c1c54aa5a0c0ad593fe97f0
CodeX-LS <80232744+CodeX-LS@users.noreply.github.com>
2023-02-03 13:09:47 +1030

    201

---
+++ full-stack/moviebrowser/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello</h1>
+     
+    </div>
+  );
+}
+
+export default App;
