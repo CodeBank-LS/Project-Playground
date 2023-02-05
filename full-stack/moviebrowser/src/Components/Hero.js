c826104d89c64d36b4321cafcaecfe410444f61a
CodeX-LS <80232744+CodeX-LS@users.noreply.github.com>
2023-02-05 20:50:28 +1030

    delete all

--- full-stack/moviebrowser/src/Components/Hero.js
+++
@@ -1,9 +0,0 @@
-const Hero=()=>{
-    return(
-        <header className="bg-dark text-white p-5">
-            <h1>This is a hero component</h1>
-        </header>
-    )
-
-}
-export default Hero;
